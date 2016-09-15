22034a43cb1f76123b06c2ae81d41eed92d49e65
Kristy <Kristy@kristys-mbp.home>
2016-09-15 17:15:04 -0400

    string date update

--- introduction_kristy_tan/src/introUnit/Procedural.java
+++ introduction_kristy_tan/src/introUnit/Procedural.java
@@ -10,7 +10,8 @@ package introUnit;
 public class Procedural {
   public static void main(String[] args){
 	String[] names = {"Jillian","Joseph","Jordan"};
-	String[] years = {"freshman", "junior". "senior"};
+	String[] years = {"freshman", "junior", "senior"};
 	System.out.println("My name is " + names[0] + " and I am a " + years[2] + ".");
+	String[] date = {"monday", "tuesday", "wednesday"};
   }
 }
