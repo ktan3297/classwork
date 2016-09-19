75ffa6bdf43b47ab554f5ae46dbee033e3655431
Student06 <Student06@10.8.33.104>
2016-09-19 13:15:17 -0400

    dataTypes 0.3

--- Introduction/src/datatypes/DataTypes.java
+++ Introduction/src/datatypes/DataTypes.java
@@ -3,8 +3,13 @@ package datatypes;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(5.0/2); 
+		System.out.println((double) 5/2); //casting the 5 into a double 
+		System.out.println(5/2); //rounded down
+		System.out.println(3+5.0/2+5*2);
+		System.out.println((double) 3.0+5/2+5*2);
+		System.out.println((int)(3.0+5)/(2+5*2));
 	}
 
 }
+
