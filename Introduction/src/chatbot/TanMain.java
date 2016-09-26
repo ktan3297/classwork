dc5c25ca27669d083834dd25179d11a527226873
Student06 <Student06@10.8.33.104>
2016-09-26 12:32:22 -0400

    caps and surrounding spaces

--- Introduction/src/chatbot/TanMain.java
+++ Introduction/src/chatbot/TanMain.java
@@ -11,6 +11,9 @@ public class TanMain {
 	
 	public static void main(String[] args) {
 		createTopics();
+//		String s1 = "a";
+//		String s2 = " 1";
+//		System.out.println(s1.compareTo(s2)); -1 means less than. period/space/num comes before a;
 		promptName();
 		talkForever();
 	}
@@ -26,7 +29,8 @@ public class TanMain {
 		while(inLoop){
 			print("Greetings, " + user+ ". How are you?");
 			response = getInput();
-			if(response.indexOf("good") <= 0){
+			// if(response.indexOf("good") >= 0){
+			if(findKeyword(response, "good", 0)){
 				print("I'm so happy you're good.");
 			}
 			else if(response.indexOf("school") >= 0){
@@ -37,6 +41,35 @@ public class TanMain {
 				print("I'm sorry. I don't understand you.");
 			}
 		} 
+	} 
+
+	public static boolean findKeyword(String searchString, String key, int startIndex) {
+		// delete white space
+		String phrase = searchString.trim(); //trim is a method included in the string api
+		//set all letters to lowercase
+		phrase = phrase.toLowerCase();
+		key = key.toLowerCase();
+		//find position of key
+		int psn = phrase.indexOf(key);
+		// keep looking for the word until you find the right context
+		while(psn >= 0){
+			String before = " ";
+			String after = " ";
+			// if the phrase does not end with this word
+			if(psn + key.length() < phrase.length()){
+				after = phrase.substring(psn + key.length(), psn + key.length() + 1).toLowerCase();
+			}
+			// if the phrase does not being with this word
+			if(psn > 0){
+				before = phrase.substring(psn - 1, psn).toLowerCase();
+			}
+			if(before.compareTo("a") < 0 && after.compareTo("a") < 0){
+				return true;
+			}
+			//incase the keyword was not found yet, check the rest of the string
+			psn = phrase.indexOf(key, psn +1); 
+		}
+		return false;
 	}
 
 	public static void promptInput() {
