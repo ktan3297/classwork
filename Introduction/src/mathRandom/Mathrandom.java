4f13e9de0deb58f056654b23f052d730a387edcd
Kristy <Kristy@192.168.1.194>
2016-09-30 21:50:42 -0400

    fixed roll dice

--- Introduction/src/mathRandom/Mathrandom.java
+++ Introduction/src/mathRandom/Mathrandom.java
@@ -5,32 +5,34 @@ import java.util.Random;
 public class Mathrandom {
 
 	public static Random genNum = new Random();
-	public static int ranNum = genNum.nextInt(6) + 1;
+	public static int ranNum = genNum.nextInt(6) + 1;	//generate random number 1-6
 	
 	public static void main(String[] args) {
 		int[] results = new int[6]; // same as int[] results ={0,0,0,0,0,0};
-		int totalRolls = 10000;
+		int totalRolls = 10000;		//Number of times to roll
 		
-		for(int index = 0; index < totalRolls; index++){
+		for(int currentRoll = 0; currentRoll < totalRolls; currentRoll++){
 			int roll = rollUnfairDie();
 			int roll2 = rollFairDie();
-			if(index % 2 == 0){
-				System.out.println("Roll #" + (index + 1) + " Die is " + roll);
+			if(currentRoll % 2 == 0){	//If odd give same number
 				results[roll-1]++;
+				System.out.println("Roll #" + (currentRoll + 1) + " Die is " + roll);
+				// roll-1 because ranNum is 1-6 and indicies are 0-5
 			}
-			else{
-				System.out.println("Roll #" + (index + 1) + " Die is " + roll2);
-				results[roll - 1]++;
+			else{	//If even give new number
+				results[roll2 - 1]++;
+				System.out.println("Roll #" + (currentRoll + 1) + " Die is " + roll2);
 			}
 		}
 		
 		for(int i = 0; i < 6; i++){
+			System.out.println("Total rolls for " + (i+1) + ": "+ results[i]);
 			int percentage = (int) (100 * ((double)results[i]/totalRolls)); // multiply by 1000 and divide by 10.0
 			System.out.println((i + 1) + " was rolled " + percentage  + " %.");
 		}
 		
 	}
-	
+	//Create a new random number
 	public static int rollFairDie(){
 		Random gen = new Random();
 		return gen.nextInt(6) + 1; //nextInt = max of 5
@@ -40,6 +42,7 @@ public class Mathrandom {
 //		return roll + 1; // because 0 becomes 1 and 5 becomes 6
 	}
 	 
+	//Return same number on instantiation of Mathrandom class
 	public static int rollUnfairDie(){
 		return ranNum;
 	}
