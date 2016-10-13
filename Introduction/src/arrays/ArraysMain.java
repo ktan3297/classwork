d8084d56ed17b6f1b1684b4425310abf4dafd6c2
Student06 <Student06@10.8.33.104>
2016-10-13 13:15:23 -0400

    arrays2

--- Introduction/src/arrays/ArraysMain.java
+++ Introduction/src/arrays/ArraysMain.java
@@ -1,15 +1,38 @@
 package arrays;
 
-public class ArraysMain {
+public class ArraysMain { // have fixed size, same types, and index
 //	array is not an object and does not have any methods
 	public static void main(String[] args) {
 //		This is how you time how quickly a computer processes
 		long startTime = System.currentTimeMillis();
-		arrayIntroMethod();
+		String[] someStrings = new String[100];
+		populateArray(someStrings);
+		
+		changeString(someStrings[99]);
+		
+		printArray(someStrings);
+		
 		long endTime = System.currentTimeMillis();
 		System.out.println("Completed method in " + (endTime - startTime) + "ms.");
 	} 
 	
+	private static void changeString(String s) {
+		s = "This string has been changed."; // why does this not work
+}
+
+	private static void printArray(String[] a) {
+		for(String s: a){
+			System.out.println(s);
+		}
+		
+	}
+
+	private static void populateArray(String[] a) {
+		for(int index = 0; index < a.length; index++){
+			a[index] = "value" + (index + 1);
+		}
+	}
+
 	public static void arrayIntroMethod(){
 //		construct 3 integer arrays
 //		these two methods do the same thing
@@ -37,7 +60,13 @@ public class ArraysMain {
 //		these two constructors are different
 		String[] strings1 = {"", "", ""};
 		String[] strings2 = new String[3];
+
+//		this loop instantiating the strings
+		for(int index = 0; index < strings2.length; index++){
+			strings2[index] = "value" + (index + 1);
+		}
 		
+//		this loop prints the string		
 		for(String s: strings2){
 			System.out.println(s);
 		}
