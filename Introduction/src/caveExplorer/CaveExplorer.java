59809b817520f8042b5b95f77afe1632ec2cee14
ktan3297 <ktan3297@bths.edu>
2016-11-10 12:53:31 -0500

    update

--- Introduction/src/caveExplorer/CaveExplorer.java
+++ Introduction/src/caveExplorer/CaveExplorer.java
@@ -8,15 +8,11 @@ public class CaveExplorer {
 	public static Scanner in;
 	public static CaveRoom currentRoom;
 	public static Inventory inventory;
-	
 	public static String[][] caveMap;
 	 
 	public static void main(String[] args) {
+		in = new Scanner(System.in);
 		caves = new CaveRoom[5][5];
-		
-		caveMap = new String[3][4];
-		createMap(caveMap);
-		printPic(caveMap);
 		
 		for(int row = 0; row < caves.length; row++){
 			for(int col = 0; col < caves[row].length; col++){
@@ -24,69 +20,29 @@ public class CaveExplorer {
 			}
 		}
 		
+		caves[0][2] = new EventRoom("This is the room where that guy with a tail met you.", new GameStartEvent());
 		currentRoom = caves[0][1];
 		currentRoom.enter(); //enter will set contents of caveroom to an x
 		caves[0][1].setConnection(CaveRoom.EAST, caves[0][2], new Door()); //shared programming protocol
 		caves[0][2].setConnection(CaveRoom.SOUTH, caves[1][2], new Door()); 
 		caves[1][2].setConnection(CaveRoom.SOUTH, caves[2][2], new Door()); 
-//		startExploring();
-		
-		
-		
+		inventory = new Inventory();
+		startExploring();
 	}
 
-	private static void startExploring() { // one does input the other does AI
+	public static void startExploring() { // one does input the other does AI
 		while(true){
+			System.out.println(inventory.getDescription());
 			System.out.println(currentRoom.getDescription());
 			System.out.println("What would you like to do?");
 			String input = in.nextLine();
 			currentRoom.interpretInput(input);
 		}
-		
-	}
-	
-	public static void createMap(String[][] map){ // every other row is |; 3 per box
-		for(int row = 0; row < map.length; row++){
-			if(row == 0){
-				for(int s = 0; s < map[row].length ; s++){
-					System.out.print(" ___");
-					if(s == map[row].length -1){
-						System.out.println("");
-					}
-				}
-			}
-			for(int col = 0; col < map[row].length ; col++){
-				
-				if(col == map[row].length -1){
-					map[row][col] = "|___|";
-				}
-				else{
-					map[row][col] = "|___";
-				}
-			}
-			
-		}
-//		for(int row = 0; row < map.length; row++){
-//			for(int col = 0; col < map[row].length ; col++){
-//				if(col == 0){
-//					
-//					map[row][col] = "|___|";
-//				}
-//				else{
-//					map[row][col] = "___|";
-//				}
-//			}
-//			
-//		}
 	}
 
-	public static void printPic(String[][] pic){
-		for(int row = 0; row < pic.length; row++){
-			for(int col = 0; col < pic[row].length; col++){
-				System.out.print(pic[row][col]);
-			}
-			System.out.println();
-		}
+	public static void print(String string) {
+		System.out.println(string);
+		
 	}
 
 }
