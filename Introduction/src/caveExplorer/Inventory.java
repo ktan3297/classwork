720e9456e74f95674fa1c15ac10ef5fbcf657829
ktan3297 <ktan3297@bths.edu>
2016-11-10 12:58:25 -0500

    inventory update

--- Introduction/src/caveExplorer/Inventory.java
+++ Introduction/src/caveExplorer/Inventory.java
@@ -2,16 +2,51 @@ package caveExplorer;
 
 public class Inventory {
 	
-	private boolean hasMap;
-	private String map;
+	public boolean hasMap;
+	public String map;
 	
 	public Inventory(){
-		hasMap = true;
+		hasMap = false;
 		updateMap();
 	}
 
-	private void updateMap() {
-		// TODO Auto-generated method stub	
+	public void updateMap() {
+		map = " ";
+		for(int i = 0; i < CaveExplorer.caves[0].length - 1; i++){
+			map += "____";
+		}
+		map += "___\n";
+		for(CaveRoom[] row: CaveExplorer.caves){
+			//three rows of text
+			for(int i = 0; i < 3; i++){
+				//a line of text for each 
+				String text = "";
+				for(CaveRoom cr: row){
+					if(cr.getDoor(CaveRoom.WEST) != null && cr.getDoor(CaveRoom.WEST).isOpen()){
+						text += " ";
+					}
+					else{
+						text += "|";
+					}
+					if(i == 0){
+						text += "   ";
+					}
+					else if(i == 1){
+						text += " " + cr.getContents() + " ";
+					}
+					else if(i == 2){
+						if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text += "   ";
+						}
+						else{
+							text += "___";
+						}
+					}
+				}//last caveroom in the row
+				text += "|";
+				map += text + "\n";
+			}//last of the 3 text lines
+		}//last row
 	}
 	
 	public String getDescription(){
